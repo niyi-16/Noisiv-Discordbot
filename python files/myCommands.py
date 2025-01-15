28092854be2c85fdb29f603dd621365b40a3b323
niyi-16 <dasil.adam@gmail.com>
2025-01-15 01:21:32 -0400

    updated functions to run off queries rather that hardcoded lists

--- python files/myCommands.py
+++ python files/myCommands.py
@@ -1,8 +1,13 @@
 import datetime
 import os
+from idlelib.query import Query
 
 import mysql.connector
+import random
 from dotenv import load_dotenv
+
+##TODO Polish code its messy!
+
 
 # dict of bot functions
 bot_functions = {
@@ -13,73 +18,82 @@ bot_functions = {
     "!list": "list all commands"
 }
 
-
-daysofweek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
-
-#            0              1            2           3            4             5
-courses = ["OSYS 1000", "PROG 2007","SAAD 1001", "PROG 2700", "PROG 1400", "ICOM 2701"]
-
-classSchedule = dict(Monday=[courses[0], courses[1], "BREAK", courses[1], courses[2]],
-                     Tuesday=[courses[1], courses[3], "BREAK", courses[3], courses[4]],
-                     Wednesday=[courses[3], courses[4], "BREAK", courses[4], courses[2]],
-                     Thursday=[courses[3], courses[0], "BREAK", courses[0], courses[5]],
-                     Friday="No Classes on Friday")
-
 def currclass() -> str:
-    """
+    load_dotenv()
 
-    :return: The Current Class based on the time
-    """
+    mydb = mysql.connector.connect(
+        host=os.getenv("HOST"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASS"),
+        database=os.getenv("DATABASE"))
 
-    current = datetime.datetime.now() # Gets info about the current day "YYYY-MM-DD HH:MM:SS.milliseconds"
-    
-    currDOW = current.strftime("%A") # Extracts day of week from current
-    currTime = float(current.strftime("%H.%M")) # Extracts the current time from current as a decimal number i.e 12:34 -> 12.34
-    td_Schdl = classSchedule[currDOW] #Uses currentDow as a key to retrieve day's schedule
-    
-    print(f"Today is a {currDOW}\n"+
-          f"The current time is {currTime}\n"+
-          f"The Classes lined up for the day are {str (td_Schdl)}\n")
+    mycursor = mydb.cursor(dictionary=True)
 
-    if 0 < currTime < 10.30: # 12AM - 10:30AM
-        return f"You are currently in {td_Schdl[0]}"
+    current = datetime.datetime.now()  # Gets info about the current day "YYYY-MM-DD HH:MM:SS.milliseconds"
 
-    elif 10.30 < currTime < 11.30: # 10:30AM - 11:30AM
-        return f"You are currently in {td_Schdl[1]}"
+    currDOW = current.strftime("%A")  # Extracts day of week from current
+    currTime = (current.strftime("%H:%M:%S"))  # Extracts the current time in specified format
 
-    elif 11.30 < currTime < 12.30: # 11:30AM - 12:30AM
-        return f"You are currently in {td_Schdl[2]}"
 
-    elif 12.30 < currTime < 13.30: #12:30AM - 1:30PM
-        return f"You are currently in {td_Schdl[3]}"
 
-    elif 13.30 < currTime < 15.30: #12:30AM - 3:30PM
-        return f"You are currently in {td_Schdl[4]}"
+    query = f"""SELECT course.code_name as 'c_code', course.name as 'c_name'
+                    from times 
+                    join course 
+                    on course.id = times.course_id 
+                    where dayOfWeek = '{currDOW}' 
+                    and '{currTime}' between startTime and endTime;"""
 
-    return "The day is over, try !nextclass, !assignments, !duedates or !commands for a list of my commands^^"
+    mycursor.execute(query)
+
+    result = mycursor.fetchone()
+
+    if result is None:
+        response = ["The day is over, try !nextclass, !assignments, !duedates or !commands for a list of my commands^^",
+                "Schools over, GO HOME!!"]
+
+        return response[random.randint(0,1)]
+
+    mycursor.close()
+    mydb.close()
+
+    return f"You currently have {result['c_code']} - {result['c_name']}"
+
 
 def nextclass():
 
+    load_dotenv()
+
+    mydb = mysql.connector.connect(
+        host=os.getenv("HOST"),
+        user=os.getenv("USER"),
+        password=os.getenv("PASS"),
+        database=os.getenv("DATABASE"))
+
+    mycursor = mydb.cursor(dictionary=True)
+
     current = datetime.datetime.now() # Gets info about the current day "YYYY-MM-DD HH:MM:SS.milliseconds"
 
-    currDOW = current.strftime("%A") # Extracts day of week from current
-    currTime = float(current.strftime("%H.%M")) # Extracts the current time from current as a decimal number i.e 12:34 -> 12.34
-    td_Schdl = classSchedule[currDOW] #Uses currentDow as a key to retrieve day's schedule
-    tmr_Schdl = classSchedule[daysofweek[1 + daysofweek.index(currDOW)]]
+    currDOW = current.strftime("%A")  # Extracts day of week from current
+    currTime = (current.strftime("%H:%M:%S"))  # Extracts the current time in specified format
 
+    query = f"""SELECT course.code_name as 'c_code', course.name as 'c_name'
+                from times
+                join course
+                on course.id = times.course_id
+                where dayOfWeek = '{currDOW}'
+                  and startTime > '{currTime}'
+                limit 1;"""
+    mycursor.execute(query)
 
-    if 0 < currTime < 8.30:
-        return f"Your next class is {td_Schdl[0]}"
-    elif 8.30 < currTime < 10.30:
-        return f"Your next class is {td_Schdl[1]}"
-    elif 10.30 < currTime < 11.30:
-        return f"Your next class is {td_Schdl[2]}"
-    elif 11.30 < currTime < 12.30:
-        return f"Your next class is {td_Schdl[3]}"
-    elif 12.30 < currTime < 13.30:
-        return f"Your next class is {td_Schdl[4]}"
-    elif currTime > 13.30:
-        return f"Your next class is {tmr_Schdl[0]}"
+    result = mycursor.fetchone()
+
+    if result is None:
+        return "Next Class is tommorow!"
+
+    #TODO some thing about the next day
+    #tmr_Schdl = classSchedule[daysofweek[1 + daysofweek.index(currDOW)]]
+
+    return f"The Next class is {result['c_code']} - {result['c_name']}"
 
 #TODO
 def assignments():
@@ -95,5 +109,5 @@ def commands():
     return item
 
 print(currclass())
-print(nextclass())
+# print(nextclass())
 print(commands())
