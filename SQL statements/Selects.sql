7e34d2ff340e179c5ebaf9bf5e40809c5836fd7e
niyi-16 <dasil.adam@gmail.com>
2025-01-15 01:18:39 -0400

    Made queries for selecting the current and next classes

--- SQL statements/Selects.sql
+++ SQL statements/Selects.sql
@@ -14,10 +14,19 @@ select dateDue, count(dateDue) as 'c' from tests
 group by dateDue
 having c >1 ;
 
-
+# Selects current class on a given day
 SELECT course.code_name, times.startTime, times.endTime
 from times
 join course
 on course.id = times.course_id
 where dayOfWeek = 'Monday'
 and '09:00:00' between startTime and endTime;
+
+# Selects next class on a given day
+SELECT course.code_name, times.startTime, times.endTime
+from times
+join course
+on course.id = times.course_id
+where dayOfWeek = 'Tuesday'
+  and startTime > '09:00:00'
+limit 1;
