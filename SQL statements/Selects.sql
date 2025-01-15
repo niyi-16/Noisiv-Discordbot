b8c122ad7270ffb99277f9be439a9cea5c4e0b66
niyi-16 <dasil.adam@gmail.com>
2025-01-15 13:50:54 -0400

    More select queries

--- SQL statements/Selects.sql
+++ SQL statements/Selects.sql
@@ -1,6 +1,8 @@
 
 SELECT * from course;
 
+select curdate(), (curdate() + interval 2 week );
+
 SELECT * from assignments
 where course_id is not null
 order by dateDue;
@@ -19,14 +21,25 @@ SELECT course.code_name, times.startTime, times.endTime
 from times
 join course
 on course.id = times.course_id
-where dayOfWeek = 'Monday'
-and '09:00:00' between startTime and endTime;
+where dayOfWeek = dayname(current_date)
+and current_time between startTime and endTime;
 
 # Selects next class on a given day
 SELECT course.code_name, times.startTime, times.endTime
 from times
 join course
 on course.id = times.course_id
-where dayOfWeek = 'Tuesday'
-  and startTime > '09:00:00'
+where dayOfWeek = dayname(current_date)
+  and startTime > current_time
 limit 1;
+
+
+SELECT course.code_name as 'c_code',
+                        assignments.assignmentName as 'a_name',
+                        DATE_FORMAT(assignments.dateDue,'%W, %D of %b')  as 'a_due',
+                        time_format(assignments.timeDue, '%h:%i') as 'a_time'
+from assignments
+join course on course.id = assignments.course_id
+where course_id is not null
+and dateDue <= (curdate() + interval 2 week)
+order by dateDue;
